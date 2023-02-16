b8e033dc53d2a6ea3b59cc1b4cc045598e135386
febias <73440637+febiasm@users.noreply.github.com>
2023-02-16 20:04:31 +0300

    Update triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if [a, b, c].any? { |side| side <= 0 }
+      raise TriangleError, "Sides must have a positive length."
+    end
+
+    sides = [a, b, c].sort
+    unless sides[0] + sides[1] > sides[2]
+      raise TriangleError, "Not a valid triangle."
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
